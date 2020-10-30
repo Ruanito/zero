ae895dde4bc4543c1ec2c5dc62404cefd364004c
Ruanito Diego Santos <ruanito.santos@ebanx.com>
2020-10-30 14:29:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const date = new Date().getTime() - new Date("October 30, 2020").getTime(); 
+  
+  const zero = Math.floor(date / (1000 * 60 * 60 * 24));
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Estamos a {zero} dias sem ninguém pedir demissão.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Nosso recorde é {zero} dias.
+        </p>
       </header>
     </div>
   );
